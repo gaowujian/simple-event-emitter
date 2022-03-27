e23558170a21f3b92fa0ecaec46f054516af349b
tony <806003097@qq.com>
2022-03-27 16:39:31 +0800

    完成基本的订阅时间和触发事件

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,5 +1,47 @@
 class EventEmitter {
-  constructor() {}
+  events: Record<string, Function[]> = {};
+  on(eventName: string, listener: Function) {
+    const existed = this.events[eventName];
+    if (!existed) {
+      this.events[eventName] = [listener];
+    } else {
+      existed.push(listener);
+    }
+  }
+  addEventListener(eventName: string, listener: Function) {
+    this.on.call(this, eventName, listener);
+  }
+  off(eventName: string, listener: Function) {
+    const listeners = this.events[eventName].filter((item) => {
+      item !== listener;
+    });
+    this.events[eventName] = listeners;
+  }
+  removeEventListener(eventName: string, listener: Function) {
+    this.off.call(this, eventName, listener);
+  }
+  once(eventName: string, listener: Function) {
+    const enhanced = () => {
+      listener();
+      this.removeEventListener(eventName, listener);
+    };
+
+    const existed = this.events[eventName];
+    if (!existed) {
+      this.events[eventName] = [enhanced];
+    } else {
+      existed.push(enhanced);
+    }
+  }
+
+  emit(eventName: string) {
+    const listeners = this.events[eventName];
+    listeners.forEach((fn) => fn());
+  }
+
+  print() {
+    console.log(this.events);
+  }
 }
 
 export default EventEmitter;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import EventEmitter from "./EventEmitter";
 export default EventEmitter;
 
-const txt: string = "hello world";
-console.log("txt:", txt);
+// const txt: string = "hello world";
+// console.log("txt:", txt);
+
+const event = new EventEmitter();
+
+const fn = () => {};
+event.addEventListener("heheh", fn);
+event.addEventListener("heheh", fn);
+event.on("xx", () => {
+  console.log("heelwer");
+});
+event.print();
+
+event.removeEventListener("heheh", fn);
+const AA = () => {
+  console.log("aa");
+};
+const BB = () => {
+  console.log("bb");
+};
+event.once("first", AA);
+
+event.emit("first");
+event.print();
