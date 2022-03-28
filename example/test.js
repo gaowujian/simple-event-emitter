a036d5d8f768ddd7bebacdda96d228f6b86a4917
tony <gwjlshebei@gmail.com>
2022-03-28 10:40:38 +0800

    update:usage demo

--- example/test.js
+++ example/test.js
@@ -1,9 +1,8 @@
 // 用于测试commonjs导出
 const EventEmitter = require("../lib/bundle");
 const event = new EventEmitter();
-
-event.addEventListener("hao", (args) => {
+event.addEventListener("test", (args) => {
   console.log("chile", ...args);
 });
 
-event.emit("hao", "aaam", "kjewr");
+event.emit("test", "aa", "bb");
