1f8849b79817d0dd411400cd59385a8b3c4d3e8e
tony <gwjlshebei@gmail.com>
2022-03-28 10:36:27 +0800

    fix:emit parameters

--- example/test.js
+++ example/test.js
@@ -1,3 +1,9 @@
 // 用于测试commonjs导出
-const data = require("../lib/bundle");
-console.log("data:", data);
+const EventEmitter = require("../lib/bundle");
+const event = new EventEmitter();
+
+event.addEventListener("hao", (args) => {
+  console.log("chile", ...args);
+});
+
+event.emit("hao", "aaam", "kjewr");
